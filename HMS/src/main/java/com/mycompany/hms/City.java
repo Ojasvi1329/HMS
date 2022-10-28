c12786076f4336c213d91d19fbf0a870227f0709
Ojasvi1329 <114348005+Ojasvi1329@users.noreply.github.com>
2022-10-27 20:34:09 -0400

    initial commits
    initial Commits

---
+++ HMS/src/main/java/com/mycompany/hms/City.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.hms;
+
+/**
+ *
+ * @author HP
+ */
+public class City {
+    private String cityname;
+
+    public String getCityname() {
+        return cityname;
+    }
+
+    public void setCityname(String cityname) {
+        this.cityname = cityname;
+    }
+    
+}
