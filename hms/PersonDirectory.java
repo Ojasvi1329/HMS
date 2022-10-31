171b9e2f18fdc20af0784c5b112c4ad6efad5908
Ojasvi1329 <114348005+Ojasvi1329@users.noreply.github.com>
2022-10-31 19:08:41 -0400

    Add files via upload
    Modifying classes

---
+++ hms/PersonDirectory.java
@@ -0,0 +1,33 @@
+package com.company.hms;
+
+import java.util.List;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Ojasvi
+ */
+public class PersonDirectory {
+    private List<Person> persons;
+
+    public PersonDirectory() {
+    }
+
+    public PersonDirectory(List<Person> persons) {
+        this.persons = persons;
+    }
+
+    public List<Person> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(List<Person> persons) {
+        this.persons = persons;
+    }
+    
+    
+}
