def4ed32d67f355d656bb401c394a7211bcb3ef7
Ojasvi1329 <114348005+Ojasvi1329@users.noreply.github.com>
2022-10-31 23:36:36 -0400

    Add files via upload
    final class

---
+++ HMS/src/main/java/com/company/hms/Doctor.java
@@ -0,0 +1,69 @@
+package com.company.hms;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ojasvi
+ */
+public class Doctor extends Person{
+    private static int count=0;
+    private String hospitalName;
+    private String hospitalDepartment;
+    private String phoneNumber;
+    private String DoctorId;
+
+    
+    public Doctor() {
+        setDoctorId("doc_"+(++count));
+        
+    }
+
+    public String getDoctorId() {
+        return DoctorId;
+    }
+
+    public void setDoctorId(String DoctorId) {
+        this.DoctorId = DoctorId;
+    }
+
+    public Doctor(String hospitalName, String hospitalDepartment, String phoneNumber) {
+        this.hospitalName = hospitalName;
+        this.hospitalDepartment = hospitalDepartment;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Doctor(String hospitalName, String hospitalDepartment, String phoneNumber, String name, String id, int age, String gender, String role, String userName, String passWord) {
+        super(name, id, age, gender, role, userName, passWord);
+        this.hospitalName = hospitalName;
+        this.hospitalDepartment = hospitalDepartment;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getHospitalName() {
+        return hospitalName;
+    }
+
+    public void setHospitalName(String hospitalName) {
+        this.hospitalName = hospitalName;
+    }
+
+    public String getHospitalDepartment() {
+        return hospitalDepartment;
+    }
+
+    public void setHospitalDepartment(String hospitalDepartment) {
+        this.hospitalDepartment = hospitalDepartment;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ HMS/src/main/java/com/company/hms/Patient.java
@@ -0,0 +1,99 @@
+package com.company.hms;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ojasvi
+ */
+public class Patient extends Person{
+    private String phoneNumber;
+    private String streetAddress;
+    private String community;
+    private String city;
+    private Long zipcode;
+    private EncounterHistory encounterHistory;
+    private String patient_id;
+    private static int count=0;
+    public Patient() {
+        setPatient_id("patient_"+(++count));
+    }
+
+    public String getPatient_id() {
+        return patient_id;
+    }
+
+    public void setPatient_id(String patient_id) {
+        this.patient_id = patient_id;
+    }
+
+    public Patient(String phoneNumber, String streetAddress, String community, String city, Long zipcode, EncounterHistory encounterHistory) {
+        this.phoneNumber = phoneNumber;
+        this.streetAddress = streetAddress;
+        this.community = community;
+        this.city = city;
+        this.zipcode = zipcode;
+        this.encounterHistory = encounterHistory;
+    }
+
+    public Patient(String phoneNumber, String streetAddress, String community, String city, Long zipcode, String name, String id, int age, String gender, String role, String userName, String password, EncounterHistory encounterHistory) {
+        super(name, id, age, gender, role, userName, password);
+        this.phoneNumber = phoneNumber;
+        this.streetAddress = streetAddress;
+        this.community = community;
+        this.city = city;
+        this.zipcode = zipcode;
+        this.encounterHistory = encounterHistory;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCommunity() {
+        return community;
+    }
+
+    public void setCommunity(String community) {
+        this.community = community;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Long getPostalCode() {
+        return zipcode;
+    }
+
+    public void setPostalCode(Long zipcode) {
+        this.zipcode = zipcode;
+    }
+    
+    public EncounterHistory getEncounterHistory() {
+        return encounterHistory;
+    }
+
+    public void setEncounterHistory(EncounterHistory encounterHistory) {
+        this.encounterHistory = encounterHistory;
+    }
+}
